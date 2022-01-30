324338a62b14e2a7c660666621d6a45eb3ada720
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2022-01-30 15:44:27 +0530

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import customColors from "../config/colors";
+
+const colors = customColors;
+const customTheme = extendTheme({ colors });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#6a4bfe",
+      primaryVariant: "#7d6bda",
+      warning: "#f6be64",
+      disabled: "#f6f8fb",
+      error: "#ff6760",
+      success: "#16d090",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
